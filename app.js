7d54d5ab4b1599797369e757b197913c45e95a2d
emanikor <87859876+emanikor@users.noreply.github.com>
2022-12-09 17:22:28 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,47 +1,29 @@
-let input = document.querySelector('.inputtext');
-let addBtn = document.querySelector('.addlist');
-let tasks = document.querySelector('.tasks');
+async function register(){
+    const registerForm = document.getElementById('registerForm');
+    const formData = new FormData(registerForm);
+    const data = object.fromentries(formData);
+    const jsonData = JSON.stringify(object.formEntries(formData));
+    const result = await createAccount(jsonData);
 
-
-//add btn disabled
-input.addEventListener('keyup', ()=> {
-    if(input.value.trim() != 0){
-        addBtn.classList.add('active')
-    } else{
-        addBtn.classList.remove('active') 
-    }
-})
-console.log(addBtn)
-//add task
-addBtn.addEventListener('click', ()=>{
-    if(input.value.trim() != 0) {
-        let newItem = document.createElement('div');
-        newItem.classList.add( 'item');
-        newItem.innerHTML = `
-        <p> ${input.value} </p>
-        <div class="items-btn"> 
-        <i class="fa-solid fa-pencil"></i>
-        <i class="fa-solid fa-xmark"></i> </div> 
-         `
-        tasks.appendChild(newItem);
-        input.value ='';
-    }else{
-        alert("enter a new task");
-    }
-
-})
-//delete item from list
-tasks.addEventListener('click', (e)=>{
-    if(e.target.classlist.contains('fa-solid fa-xmark')){
-        e.target.parentElement.parentElement.remove();
-    }
-})  
-
-//mark item complete
- tasks.addEventListener('click',(e)=>{
-    if(e.target.classList.contains('fa-solid fa-pencil')
-    );
-{
-    e.target.parentElement.parentElement.classList.toggle('completed');
+ if(result.error){
+    return console.log('An error occurred:', result.error);
+ }
+ console.log('account created!',result);
 }
- })
+
+async function creatAccount(account) {
+    try{
+        const response = await fetch('//localhost:5000/api/accounts', {
+            method:'post',
+            headers: {'content-type': 'application/json'},
+            body: account
+
+        });
+        return await response.json();
+
+    }catch(error){
+        return{
+            error: Error.message || 'unknown error '
+        };
+    }
+}

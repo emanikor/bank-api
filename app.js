36fa407a07fb368cf19f715e5da6aca009d64a0f
emanikor <87859876+emanikor@users.noreply.github.com>
2023-02-24 23:08:11 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,29 +1,36 @@
-const routes ={
-    '/login': {templateId: 'login'},
-    '/dashboard': {templateId:'dashboard'},
-};
+const routes = {
+    '/login': { templateId: 'login' },
+    '/dashboard': { templateId: 'dashboard' },
+  };
+  
 
-function updateRoute(){
-    const path =window.location.pathname;
+function updateRoute(templateId) {
+    const path = window.location.pathname;
     const route = routes[path];
 
-    const template = document.getElementById(route.templateId);
-    const view = template.content.cloneNode(true);
-    const app =document.getElementById('app');
-    app.innerHTML ='';
-    app.appendChild(view);
-    if(!route){
+    if (!route) {
         return navigate('/login');
-    }
+      }
+    
+    const template = document.getElementById(templateId);
+    const view = template.content.cloneNode(true);
+    const app = document.getElementById('app');
+    app.innerHTML = '';
+    app.appendChild(view);
+  }
+  
+  updateRoute('login');
+  
 
-}
-
-function onLinkClink(event){
+  function onLinkClick(event) {
     event.preventDefault();
     navigate(event.target.href);
-}
+  }
+  
+  
+
 function navigate(path){
-    window.history.pushstate({}, path,path);
+    window.history.pushState({}, path, path);
     updateRoute();
 }
 
@@ -31,70 +38,120 @@ function rBtn(event){
     event.console.log()
     
 }
+//navigate user account
+// async function getAccount(user) {
+//     try{
+//         const response = await fetch('//localhost:5500/api/accounts', + encodeURIComponent(user));
+//         return await response.json();
+//         }catch(error){
+//             return{error:error.message || 'unknown error'};
+//         }
+// }
 
-async function getAccount(user) {
-    try{
-        const response = await fetch('//localhost:5500/api/accounts', + encodeURIComponent(user));
-        return await response.json();
-        }catch(error){
-            return{error:error.message || 'unknown error'};
-        }
-}
+
 //to get useraccount
-async function login(){
-   const loginForm = document.getElementById('login')
-   const user = loginForm.user.value;
-   const data =await getAccount(user);
+// async function login(){
+//    const loginForm = document.getElementById('login')
+//    const user = loginForm.user.value;
+//    const data =await getAccount(user);
 
- if(data.error){
-    return console.log('loginError', data.error);
+//  if(data.error){
+//     return console.log('loginError', data.error);
 
- }
- account = data;
- navigate('/dashboard');
+//  }
+//  account = data;
+//  navigate('/dashboard');
 
-}
+// }
 
 
 
 //prepare data to be send to the server
-async function register(){
+// function register() {
+//     const registerForm = document.getElementById('registerForm');
+//     const formData = new FormData(registerForm);
+//     const data = Object.fromEntries(formData);
+//     const jsonData = JSON.stringify(data);
+//   }
+
+ 
+async function register() {
+    
     const registerForm = document.getElementById('registerForm');
-    const formData = new FormData(registerForm);
-    const data = object.fromentries(formData);
-    const jsonData = JSON.stringify(object.formEntries(data));
-    const result = await createAccount(jsonData);
-    const regBtn = document.getElementById('btn-reg')
+  const formData = new FormData(registerForm);
+  const jsonData = JSON.stringify(Object.fromEntries(formData));
+  const result = await createAccount(jsonData);
 
-regBtn.addEventListener('click',function(){
-     console.log(register)
-})
-
- if(result.error){
-    return console.log('An error occurred:', result.error);
- }
- console.log('account created!',result);
- console.log('hello')
+  
+if(result.error){
+    return console.log('an error occured:', result.error);
 }
+console.log('Account Created!', result);
+}
+
+
+// async function register(){
+//     const regBtn = document.getElementById('btn-reg');
+//     const registerForm = document.getElementById('registerForm');
+//     const formData = new FormData(registerForm);
+//     const data = object.fromentries(formData);
+//     const jsonData = JSON.stringify(object.formEntries(data));
+//     const result = await createAccount(jsonData);
+ 
+
+// regBtn.addEventListener('click',function(){
+//     if(reg==)
+//      console.log(register)
+// })
+
+//  if(result.error){
+//     return console.log('An error occurred:', result.error);
+//  }
+//  console.log('account created!',result);
+//  console.log('hello')
+// }
 
 //async wait /*waiting for the server response*/
-
-async function creatAccount(account) {
+async function createAccount(account){
     try{
-        const response = await fetch('//localhost:5000/api/accounts', {
-            method:'post',
-            headers: {'content-type': 'application/json'},
-            body: account
+        const response = await fetch('//localhost:5000/api/accounts',
+    {
+        method :'post',
+        headers: { 'content-type': 'application/json'},
+        body:account
+    });
+    return await response.json();
 
-        });
-        return await response.json();
-
-    }catch(error){
-        return{
-            error: Error.message || 'unknown error '
-        };
     }
-}
+    catch(error) {
+        return{
+            error: error.message || 'unknown error'};
+        }
+    }
+
+
+
+
+// async function creatAccount(account) {
+//     try{
+//         const response = await fetch('//localhost:5000/api/accounts', {
+//             method:'post',
+//             headers: {'content-type': 'application/json'},
+//             body: account
+
+//         });
+//         return await response.json();
+
+//     }catch(error){
+//         return{
+//             error: Error.message || 'unknown error '
+//         };
+//     }
+// }
+
+
 window.onpopstate = () => updateRoute();
 updateRoute();
+
+
 
